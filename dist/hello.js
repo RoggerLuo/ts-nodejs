96aef421dd4f774df4a8b398e986c17935329008
rogerFromSAMA <luojie.5408@163.com>
2019-09-17 23:44:35 +0800

    init

---
+++ dist/hello.js
@@ -0,0 +1,17 @@
+"use strict";
+// server.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const express = require("express");
+var express_1 = __importDefault(require("express")); // lib es7
+var app = express_1.default();
+app.get('/', function (req, res) {
+    console.log(123);
+    res.send('hello world!');
+});
+var server = app.listen(3001, function () {
+    console.log('Listen on port 3001');
+});
+//# sourceMappingURL=hello.js.map
---
+++ src/hello.ts
@@ -0,0 +1,13 @@
+// server.ts
+
+// const express = require("express");
+import express from "express" // lib es7
+
+const app = express();
+app.get('/', function (req: any, res: { send: (arg0: any) => void; }) {
+    console.log(123);
+    res.send('hello world!');
+});
+const server = app.listen(3001, function () {
+    console.log('Listen on port 3001');
+});
